f19b38fe3a4b9e810be3b489ccd53d843a6b2f81
jakethechanger <jakethechanger@panacea-coin.org>
2020-03-28 08:05:21 +0700

    imac

--- restapi/rest-api-workshop/src/routes/person.js
+++ restapi/rest-api-workshop/src/routes/person.js
@@ -14,22 +14,22 @@ let router = express.Router()
 
 //QueryString => Query property on the request object
 //localhost:3000/person?name=thomas //single query
-// router.get('/person', (req, res) => {
-//     if(req.query.name){
-//         res.send(`You have requested a person ${req.query.name}`)
-//     }
-//     else {
-//         res.send('You have requested a person')
-//     }
-// })
-//QueryString => Query property on the request object
-//localhost:3000/person?name=thomas&age=20 //double query
 router.get('/person', (req, res) => {
     if(req.query.name){
-        res.send(`You have requested a person ${req.query.name} ${req.query.age}`)
+        res.send(`You have requested a person ${req.query.name}`)
     }
     else {
         res.send('You have requested a person')
     }
 })
+//QueryString => Query property on the request object
+//localhost:3000/person?name=thomas&age=20 //double query
+// router.get('/person', (req, res) => {
+//     if(req.query.name){
+//         res.send(`You have requested a person ${req.query.name} ${req.query.age}`)
+//     }
+//     else {
+//         res.send('You have requested a person')
+//     }
+// })
 module.exports = router
